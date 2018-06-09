d7f65a9da25b6a6e350f33d481060ce702a05405
Unknown <geosohal@gmail.com>
2018-06-08 22:54:31 -0700

    space station update 2

--- SpaceStation.cs
+++ SpaceStation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BuildingType
 {
@@ -8,32 +9,317 @@ public enum BuildingType
     Disk,
     Disk2,
     SmallDisk,      // also a hub but only connects small bridges
-    Room
+    Room,      
+}
+
+public enum HorizontalBuildingType
+{
+    LargeBridge,
+    LargeBridgeDouble,
+    SmallBridge,
+    SmallBridgeDouble,
+    Turret // can only be built of main connectors at the base height
+           //one design option is to allow them on main connectors of other heights but not now
 }
 
 public class SpaceStation : MonoBehaviour {
 
+    // set by unity:
+    public GameObject UpdateText;
+
+    // STATIC GLOBALS:
     // heights of buildings are from top to bottom, the hub connectors are halfway between top and bottom
     // small disk should be slightly less than half but will work for now.
     public static Dictionary<BuildingType, float> buildingHeightTable;
     public static float bigBridgeLength = 162f;
+  //  public static float smallBridgeLength = 122f;//deperecated - needs to be same as other bridge
     public static float maxHeightTop = 30f;
     public static float maxHeightBtm = -200f;
+
+    // OTHER MEMBERS:
+    public StationNode selectedNode;
+    private int gridSize = 20;  // should keep this an even number
+    public StationNode[,] stationGrid;
+    private Tuple<int, int> currentStation;
+    public bool IsEmpty;
+    private bool buildOnBottom; // if false then builder is set to build on top
+    private Direction selectedDir;
+
+
+    public SpaceStation()
+    {
+    }
+
 	// Use this for initialization
 	void Start () {
-		if (buildingHeightTable == null)
+		if (SpaceStation.buildingHeightTable == null)
         {
-            buildingHeightTable = new Dictionary<BuildingType, float>();
-            buildingHeightTable.Add(BuildingType.SmallDisk, 20f);
-            buildingHeightTable.Add(BuildingType.Room, 53f);
-            buildingHeightTable.Add(BuildingType.MainConnector, 25f);
-            buildingHeightTable.Add(BuildingType.Disk, 18f);
-            buildingHeightTable.Add(BuildingType.Disk2, 25f);
+            Debug.Log("init height table");
+            SpaceStation.buildingHeightTable = new Dictionary<BuildingType, float>();
+            SpaceStation.buildingHeightTable.Add(BuildingType.SmallDisk, 20f);
+            SpaceStation.buildingHeightTable.Add(BuildingType.Room, 53f);
+            SpaceStation.buildingHeightTable.Add(BuildingType.MainConnector, 25f);
+            SpaceStation.buildingHeightTable.Add(BuildingType.Disk, 18f);
+            SpaceStation.buildingHeightTable.Add(BuildingType.Disk2, 25f);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        stationGrid = new StationNode[gridSize / 2,gridSize / 2];
+        currentStation = new Tuple<int, int>(gridSize / 2, gridSize / 2);
+        IsEmpty = true;
+        buildOnBottom = true;
+
+        ((Button)GameObject.FindGameObjectWithTag("btnBigDisk").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnSmDisk").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnRoom").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnHub").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnNorth").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnEast").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnSouth").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnWest").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnBigLongBridge").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnThinLongBridge").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnThinBridge").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnBigBridge").GetComponent<Button>()).interactable = false;
+
+
+        //((Button)GameObject.FindGameObjectWithTag("btnBigDisk").GetComponent<Button>()).interactable = false;
+        //((Button)GameObject.FindGameObjectWithTag("btnSmDisk").GetComponent<Button>()).interactable = false;
+        //((Button)GameObject.FindGameObjectWithTag("btnRoom").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnHub").GetComponent<Button>()).onClick.AddListener(() => this.OnHubButton());
+        ((Button)GameObject.FindGameObjectWithTag("btnTop").GetComponent<Button>()).onClick.AddListener(() => this.OnTopSelected());
+        ((Button)GameObject.FindGameObjectWithTag("btnBottom").GetComponent<Button>()).onClick.AddListener(() => this.OnBottomSelected());
+        ((Button)GameObject.FindGameObjectWithTag("btnNorth").GetComponent<Button>()).onClick.AddListener(() => this.OnDirectionSelect(Direction.North));
+        ((Button)GameObject.FindGameObjectWithTag("btnEast").GetComponent<Button>()).onClick.AddListener(() => this.OnDirectionSelect(Direction.East));
+        ((Button)GameObject.FindGameObjectWithTag("btnSouth").GetComponent<Button>()).onClick.AddListener(() => this.OnDirectionSelect(Direction.South));
+        ((Button)GameObject.FindGameObjectWithTag("btnWest").GetComponent<Button>()).onClick.AddListener(() => this.OnDirectionSelect(Direction.West));
+        ((Button)GameObject.FindGameObjectWithTag("btnBigLongBridge").GetComponent<Button>()).interactable = false;
+        //((Button)GameObject.FindGameObjectWithTag("btnThinLongBridge").GetComponent<Button>()).interactable = false;
+        //((Button)GameObject.FindGameObjectWithTag("btnThinBridge").GetComponent<Button>()).interactable = false;
+        //((Button)GameObject.FindGameObjectWithTag("btnBigBridge").GetComponent<Button>()).interactable = false;
+
+        ((Button)GameObject.FindGameObjectWithTag("btnUp").GetComponent<Button>()).onClick.AddListener(() => this.OnUpPress());
+        ((Button)GameObject.FindGameObjectWithTag("btnDown").GetComponent<Button>()).onClick.AddListener(() => this.OnDownPress());
+    }
+
+    public void CreateFirstHub(Vector3 pos)
+    {
+        StationNode newNode = new StationNode();
+        newNode.Initialize(true, pos);
+        stationGrid[currentStation.first, currentStation.second] = newNode;
+        selectedNode = newNode;
+    }
+
+    public void OnHubButton()
+    {
+        if (IsEmpty)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject player in players)
+            {
+                if (((ItemBehaviour)player.GetComponent<ItemBehaviour>()).item.IsMine)
+                {
+                    Vector3 playerPos = player.transform.position;
+                    CreateFirstHub(playerPos);
+                    OnBottomSelected();
+                    continue;
+                }
+            }
+        }
+        else
+        {
+
+
+            // note that any hubs we add, besides the first - on a new tower, will ned to check adjacent grid squares to see if they are covered by a bridge
+            // or if they are also a hub with the adjacent port used - in both cases it means the new hubs port will need to be set to used upon creation
+        }
+    }
+
+    public void OnBigLongBridgeButton()
+    {
+        selectedNode.BuildBridgeOnSelectedHub(selectedDir,null);
+        // also need to fill in empty grid slots so that we know the grid slot is covered by a bridge
+            // note that any hubs we add, besides the first, will ned to check adjacent grid squares to see if they are covered by a bridge
+            // or if they are also a hub with the adjacent port used - in both cases it means the new hubs port will need to be set to used upon creation
+        stationGrid[currentStation.first + DirectionEastWest(selectedDir), currentStation.second + DirectionNorthSouth(selectedDir)].
+    }
+
+
+
+    // update the N S E W buttons to show what ports are allowed
+    public void OnHubSelect()
+    {
+        // note: to build a node out of a hub's open port you first must build a bridge
+        //  once there is a bridge then an option appears to build a node (unless one was already there)
+        // double length bridges are allowed
+
+        EnableDirections();
+        foreach (var port in selectedNode.selectedHub.portsUsed)
+        {
+            DisableDirection(port.first);
+        }
+
+    }
+
+    public void OnUpPress()
+    {
+        if (stationGrid[currentStation.first, currentStation.second].SelectNextHubUp())
+            OnHubSelect();
+    }
+
+    public void OnDownPress()
+    {
+        if (stationGrid[currentStation.first, currentStation.second].SelectNextHubDown())
+            OnHubSelect();
+    }
+
+    // after a port has been selected, update the bridge buttons to show what bridge types are allowed
+    public void OnDirectionSelect(Direction dir)
+    {
+        // one case where a bridge type wouldnt be allowed is where you try to do a long bridge over a node
+        // another case is where you do a bridge and instead of connecting to a node on the other side it 
+        // connects to some other building type
+        // so we just need to check for these cases:
+
+        StationNode nodeToTest = GetStationInDirection(dir, 1);
+        float heightOfSelectedHub = stationGrid[currentStation.first, currentStation.second].selectedHub.pos.y - .5f;
+        bool canStepByOne = false;
+        
+        // test for non-long bridges
+        if (nodeToTest != null)
+        {
+            StationBuilding potentialHub = nodeToTest.GetBuildingTypeAtHeight(heightOfSelectedHub);
+            canStepByOne = true;
+            if (potentialHub == null)
+            {
+                // okay to build any bridge
+                ((Button)GameObject.FindGameObjectWithTag("btnThinBridge").GetComponent<Button>()).interactable = true;
+                ((Button)GameObject.FindGameObjectWithTag("btnBigBridge").GetComponent<Button>()).interactable = true;
+                selectedDir = dir;
+            }
+            else if (potentialHub.type == BuildingType.MainConnector)
+            {
+                // okay to build thick bridge todog
+                ((Button)GameObject.FindGameObjectWithTag("btnBigBridge").GetComponent<Button>()).interactable = true;
+                selectedDir = dir;
+            }
+            else if (potentialHub.type == BuildingType.SmallDisk)
+            {
+                // okay to build thin bridge
+                ((Button)GameObject.FindGameObjectWithTag("btnThinBridge").GetComponent<Button>()).interactable = true;
+                selectedDir = dir;
+            }
+            else
+                canStepByOne = false;
+
+        }
+        else
+        {
+            // can't build any bridge because this is off grid range
+            UpdateText.GetComponent<Text>().text = "can't build any bridge because this is off grid range  ";
+        }
+
+        // now test the same for long bridges
+        nodeToTest = GetStationInDirection(dir, 2);
+        if (nodeToTest != null && canStepByOne)
+        {
+            StationBuilding potentialHub = nodeToTest.GetBuildingTypeAtHeight(heightOfSelectedHub);
+            if (potentialHub == null)
+            {
+                // okay to build any long bridge
+                ((Button)GameObject.FindGameObjectWithTag("btnBigLongBridge").GetComponent<Button>()).interactable = true;
+                ((Button)GameObject.FindGameObjectWithTag("btnThinLongBridge").GetComponent<Button>()).interactable = true;
+
+            }
+            else if (potentialHub.type == BuildingType.MainConnector)
+            {
+                // okay to build thick longbridge todog
+                ((Button)GameObject.FindGameObjectWithTag("btnBigLongBridge").GetComponent<Button>()).interactable = true;
+            }
+            else if (potentialHub.type == BuildingType.SmallDisk)
+            {
+                // okay to build thin long bridge
+                ((Button)GameObject.FindGameObjectWithTag("btnThinLongBridge").GetComponent<Button>()).interactable = true;
+            }
+
+        }
+        else
+        {
+            // can't build any bridge because this is off grid range
+            UpdateText.GetComponent<Text>().text = "can't build any long bridge because this is off grid range  ";
+        }
+
+    }
+
+    // after bottom button has been selected update the station building buttons that are allowed
+    public void OnBottomSelected()
+    {
+        bool buildable = stationGrid[currentStation.first, currentStation.second].GetBottomHeight() > maxHeightBtm;
+        if (buildable)
+        {
+            // make all buildings buildable
+            ((Button)GameObject.FindGameObjectWithTag("btnBigDisk").GetComponent<Button>()).interactable = true;
+            ((Button)GameObject.FindGameObjectWithTag("btnSmDisk").GetComponent<Button>()).interactable = true;
+            ((Button)GameObject.FindGameObjectWithTag("btnRoom").GetComponent<Button>()).interactable = true;
+            ((Button)GameObject.FindGameObjectWithTag("btnHub").GetComponent<Button>()).interactable = true;
+
+            buildOnBottom = true;
+        }
+    }
+
+    public void OnTopSelected()
+    {
+        buildOnBottom = false;
+        float topHeight = stationGrid[currentStation.first, currentStation.second].GetTopHeight();
+        if (topHeight + buildingHeightTable[BuildingType.Disk] < maxHeightTop)
+        {
+            // disk is buildable
+            ((Button)GameObject.FindGameObjectWithTag("btnBigDisk").GetComponent<Button>()).interactable = true;
+        }
+        if (topHeight + buildingHeightTable[BuildingType.SmallDisk] < maxHeightTop)
+        {
+            // sm disk is buildable
+            ((Button)GameObject.FindGameObjectWithTag("btnSmDisk").GetComponent<Button>()).interactable = true;
+        }
+        if (topHeight + buildingHeightTable[BuildingType.Room] < maxHeightTop)
+        {
+            // room is buildable
+            ((Button)GameObject.FindGameObjectWithTag("btnRoom").GetComponent<Button>()).interactable = true;
+        }
+        if (topHeight + buildingHeightTable[BuildingType.MainConnector] < maxHeightTop)
+        {
+            // main connector is buildable
+            ((Button)GameObject.FindGameObjectWithTag("btnHub").GetComponent<Button>()).interactable = true;
+        }
+    }
+
+    private StationNode GetStationInDirection(Direction dir, int amount)
+    {
+        int x = currentStation.first + DirectionEastWest(dir) * amount;
+        int y = currentStation.second + DirectionNorthSouth(dir) * amount;
+        if (x < 0 || y < 0 || x >= gridSize || y >= gridSize)
+            return null;
+        return stationGrid[x, y];
+    }
+
+    private int DirectionEastWest(Direction dir)
+    {
+        if (dir == Direction.East)
+            return 1;
+        if (dir == Direction.West)
+            return -1;
+        return 0;
+    }
+    private int DirectionNorthSouth(Direction dir)
+    {
+        if (dir == Direction.North)
+            return 1;
+        if (dir == Direction.South)
+            return -1;
+        return 0;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -43,5 +329,44 @@ public class SpaceStation : MonoBehaviour {
             return true;
         return false;
     }
+
+// GUI helpers
+    private void EnableDirections()
+    {
+        ((Button)GameObject.FindGameObjectWithTag("btnNorth").GetComponent<Button>()).interactable = true;
+        ((Button)GameObject.FindGameObjectWithTag("btnEast").GetComponent<Button>()).interactable = true;
+        ((Button)GameObject.FindGameObjectWithTag("btnSouth").GetComponent<Button>()).interactable = true;
+        ((Button)GameObject.FindGameObjectWithTag("btnWest").GetComponent<Button>()).interactable = true;
+    }
+
+    private void DisableBridges()
+    {
+        ((Button)GameObject.FindGameObjectWithTag("btnBigLongBridge").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnThinLongBridge").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnThinBridge").GetComponent<Button>()).interactable = false;
+        ((Button)GameObject.FindGameObjectWithTag("btnBigBridge").GetComponent<Button>()).interactable = false;
+    }
+
+    private void DisableDirection(Direction dir)
+    {
+        if (dir == Direction.North)
+        {
+            ((Button)GameObject.FindGameObjectWithTag("btnNorth").GetComponent<Button>()).interactable = false;
+        }
+        if (dir == Direction.East)
+        {
+            ((Button)GameObject.FindGameObjectWithTag("btnEast").GetComponent<Button>()).interactable = false;
+        }
+        if (dir == Direction.South)
+        {
+            ((Button)GameObject.FindGameObjectWithTag("btnSouth").GetComponent<Button>()).interactable = false;
+        }
+        if (dir == Direction.West)
+        {
+            ((Button)GameObject.FindGameObjectWithTag("btnWest").GetComponent<Button>()).interactable = false;
+        }
+    }
+
+
 
 }
--- StationNode.cs
+++ StationNode.cs
@@ -2,62 +2,122 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction
+{
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
+}
 
 
+public class StationBuilding
+{
+    public BuildingType type;
+    public List<Tuple<Direction, StationBuilding>> portsUsed;   // ports that are already used and cannot be further connected
+    public Vector3 pos;
+    public StationNode parentNode;
+
+    //public StationBuilding()
+    //{
+    //    type = BuildingType.MainConnector;
+    //    portsUsed = new List<Tuple<Direction, StationBuilding>>();
+    //}
+
+    //public StationBuilding(BuildingType type, Vector3 pos)
+    //{
+    //    this.type = type;
+    //    this.pos = pos;
+    //    portsUsed = new List<Tuple<Direction, StationBuilding>>();
+    //}
+
+    public StationBuilding(BuildingType type, Vector3 pos, StationNode parent)
+    {
+        this.type = type;
+        this.pos = pos;
+        portsUsed = new List<Tuple<Direction, StationBuilding>>();
+        parentNode = parent;
+    }
+
+    public bool IsPortUsed(Direction dir)
+    {
+        foreach (var port in portsUsed)
+        {
+            if (port.first == dir)
+                return true;
+        }
+        return false;
+    }
+}
+
+
+// a station node contains a vertical tower of buildings
 public class StationNode : MonoBehaviour {
 
-    public enum Direction
-    {
-        North = 1,
-        East = 2,
-        South = 3,
-        West = 4
-    }
 
-    class StationBuilding
-    {
-        public BuildingType type;
-        public List<Direction> portsUsed;   // ports that are already used and cannot be further connected
-        public Vector3 pos;
-
-        public StationBuilding()
-        {
-            type = BuildingType.MainConnector;
-            portsUsed = new List<Direction>();
-        }
-
-        public StationBuilding(BuildingType type, Vector3 pos)
-        {
-            this.type = type;
-            this.pos = pos;
-            portsUsed = new List<Direction>();
-        }
-    }
 
     LinkedList<StationBuilding> buildings;  // first is top, last is bottom
     LinkedList<StationBuilding> hubBuildings;
-    int topLevel;   // current height of top most building
+    //int topLevel;   // current height of top most building--- dont need anymore we now use SpaceStation.maxHeightTop to limit height
     private int maxTop = 2;
-    StationBuilding selectedHub;
+    public StationBuilding selectedHub;
+
+    public StationNode()
+    {
+        buildings = new LinkedList<StationBuilding>();
+        hubBuildings = new LinkedList<StationBuilding>();
+        selectedHub = null;
+    }
 
     // hubs used port is the occupied port for this newly initialized node's hub.
-    public void Initialize(bool isBigHub, Direction hubsUsedPort, Vector3 pos)
+    public void Initialize(bool isBigHub, Tuple<Direction, StationBuilding> hubsUsedPort, Vector3 pos)
     {
-        StationBuilding mainBuilding;
+        Initialize(isBigHub, pos);
+        selectedHub.portsUsed.Add(hubsUsedPort);
+    }
+
+    // use this initialize for the first node of a space station which has no hubports used
+    public void Initialize(bool isBigHub, Vector3 pos)
+    {
         if (isBigHub)
         {
-            mainBuilding = new StationBuilding(BuildingType.MainConnector, pos);
+            selectedHub = new StationBuilding(BuildingType.MainConnector, pos, this);
         }
         else
         {
-            mainBuilding = new StationBuilding(BuildingType.SmallDisk, pos);
+            selectedHub = new StationBuilding(BuildingType.SmallDisk, pos, this);
+
         }
 
-        buildings.AddFirst(mainBuilding);
-        hubBuildings.AddFirst(mainBuilding);
+        buildings.AddFirst(selectedHub);
+        hubBuildings.AddFirst(selectedHub);
+    }
 
-        mainBuilding.portsUsed.Add(hubsUsedPort);
-        selectedHub = mainBuilding;
+    public bool IsEmpty() { return buildings.Count > 0; }
+
+    public float GetBottomHeight()
+    {
+        return buildings.Last.Value.pos.y;
+    }
+
+    public float GetTopHeight()
+    {
+        return buildings.First.Value.pos.y;
+    }
+
+    public bool CanAddBuilding(bool top, BuildingType btype, Vector3 buildingPos, float newBuildingHeight)
+    {
+        if (top)
+        {
+            if (buildingPos.y + newBuildingHeight > SpaceStation.maxHeightTop)
+                return false;
+        }
+        else
+        {
+            if (buildingPos.y - newBuildingHeight < SpaceStation.maxHeightBtm)
+                return false;
+        }
+        return true;
     }
 
     public bool AddBuilding(bool top, BuildingType btype)
@@ -66,10 +126,10 @@ public class StationNode : MonoBehaviour {
         if (top)
         {
             Vector3 buildingPos = buildings.First.Value.pos;
-            if (buildingPos.y + newBuildingHeight > SpaceStation.maxHeightTop)
+            if (CanAddBuilding(top,btype,buildingPos,newBuildingHeight))
                 return false;
             buildingPos.y += newBuildingHeight;
-            StationBuilding newBuilding = new StationBuilding(btype, buildingPos);
+            StationBuilding newBuilding = new StationBuilding(btype, buildingPos, this);
             buildings.AddFirst(newBuilding);
             if (SpaceStation.IsHub(btype))
                 hubBuildings.AddFirst(newBuilding);
@@ -77,10 +137,10 @@ public class StationNode : MonoBehaviour {
         else
         {
             Vector3 buildingPos = buildings.Last.Value.pos;
-            if (buildingPos.y - newBuildingHeight < SpaceStation.maxHeightBtm)
+            if (CanAddBuilding(top, btype, buildingPos, newBuildingHeight))
                 return false;
             buildingPos.y -= SpaceStation.buildingHeightTable[buildings.Last.Value.type];
-            StationBuilding newBuilding = new StationBuilding(btype, buildingPos);
+            StationBuilding newBuilding = new StationBuilding(btype, buildingPos, this);
             buildings.AddLast(newBuilding);
             if (SpaceStation.IsHub(btype))
                 hubBuildings.AddLast(newBuilding);
@@ -89,38 +149,94 @@ public class StationNode : MonoBehaviour {
 
     }
 
-    public List<Direction> GetPortsUsedOnSelectedHub()
+  
+    public StationBuilding GetBuildingTypeAtHeight(float height)
+    {
+        // remember positions of buildings are measured from their ceiling, so a building
+        // is at a certain height if its less than pos.y but greater than pos.y-buildingheight
+        foreach (var building in buildings)
+        {
+            if (height < building.pos.y && height > building.pos.y - SpaceStation.buildingHeightTable[building.type])
+                return building;
+        }
+        return null;
+    }
+
+    public List<Tuple<Direction,StationBuilding>> GetPortsUsedOnSelectedHub()
     {
         return selectedHub.portsUsed;
     }
 
+    public bool CanSelectUp()
+    {
+        if (hubBuildings.Find(selectedHub).Previous == null)
+            return false;
+        return true;
+    }
+
+    public bool CanSelectDown()
+    {
+        if (hubBuildings.Find(selectedHub).Next == null)
+            return false;
+        return true;
+    }
     // returns false if no hub is available going up
     public bool SelectNextHubUp()
     {
-        if (hubBuildings.Find(selectedHub).Previous == null)
+        if (CanSelectUp())
             return false;
         else
             selectedHub = hubBuildings.Find(selectedHub).Previous.Value;
         return true;
     }
 
+
     // returns false if no hub is available going down
     public bool SelectNextHubDown()
     {
-        if (hubBuildings.Find(selectedHub).Next == null)
+        if (CanSelectDown())
             return false;
         else
             selectedHub = hubBuildings.Find(selectedHub).Next.Value;
         return true;
     }
 
-    public bool BuildBridgeOnSelectedHub(Direction dir)
+    // user of function ensures that correct bridge type is used for the building
+    public bool BuildBridgeOnSelectedHub(Direction dir, StationBuilding connectedNode)
     {
-        if (selectedHub.portsUsed.Contains(dir))
+        if (selectedHub.IsPortUsed(dir))
             return false;
-        
-        f
+
+        selectedHub.portsUsed.Add( new Tuple<Direction, StationBuilding>(dir,connectedNode) );
         return true;
+    }
+
+    public Vector3 GetNextNodePosFromSelectedHub(Direction dir, HorizontalBuildingType hBuildingType)
+    {
+        float bridgeLen;
+        if (hBuildingType == HorizontalBuildingType.LargeBridge || hBuildingType == HorizontalBuildingType.SmallBridge)
+            bridgeLen = SpaceStation.bigBridgeLength;
+        else if (hBuildingType == HorizontalBuildingType.SmallBridgeDouble || hBuildingType == HorizontalBuildingType.LargeBridgeDouble)
+            bridgeLen = SpaceStation.bigBridgeLength * 2;
+        else //if (hBuildingType == HorizontalBuildingType.Turret)
+            return Vector3.zero;
+
+        Vector3 ans;
+        if (selectedHub.IsPortUsed(dir))
+        {
+            if (dir == Direction.North)
+                ans = selectedHub.pos + bridgeLen * Vector3.forward;
+            else if (dir == Direction.East)
+                ans = selectedHub.pos + bridgeLen * Vector3.right;
+            else if (dir == Direction.South)
+                ans = selectedHub.pos + bridgeLen * Vector3.back;
+            else if (dir == Direction.West)
+                ans = selectedHub.pos + bridgeLen * Vector3.left;
+            else
+                ans = Vector3.zero;
+            return ans;
+        }
+        return Vector3.zero;
     }
 
 
